c758998c1cc46759a5c0b8b78800d801ef023ec5
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-07-03 11:48:06 +0530

    add header and login

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,7 @@
+export default function IndexPages() {
+  return (
+    <div>
+      <h1>Index Page</h1>
+    </div>
+  );
+}
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,41 @@
+import { Link, useState } from "react-router-dom";
+
+export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form action="" className="max-w-md mx-auto border">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            All ready member?{" "}
+            <Link to={"/login"} className="underline text-bn">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
