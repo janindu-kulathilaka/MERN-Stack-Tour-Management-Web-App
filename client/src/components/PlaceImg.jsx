e33444677ac69eb41f6d6c6cbcaaae1b4c99970a
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-07-11 02:55:20 +0530

    setup booking page

---
+++ client/src/components/PlaceImg.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export default function PlaceImg({ place, index = 0, className = null }) {
+  if (!place.photos?.length) {
+    return "";
+  }
+
+  if (!className) {
+    className = "object-cover";
+  }
+
+  return (
+    <img
+      className={className}
+      src={"http://localhost:4000/uploads/" + place.photos[index]}
+      alt="place"
+    />
+  );
+}
+
+PlaceImg.propTypes = {
+  place: PropTypes.object.isRequired,
+  index: PropTypes.number,
+  className: PropTypes.string,
+};
--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import AccountNav from "../components/AccountNav";
 import axios from "axios";
+import PlaceImg from "../components/PlaceImg";
 
 export default function BookingPage() {
   const [bookings, setBookings] = useState([]);
@@ -17,7 +18,14 @@ export default function BookingPage() {
       <div>
         {bookings?.length > 0 &&
           bookings.map((booking) => (
-            <div key={booking}>
+            <div
+              key={booking}
+              className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden"
+            >
+              <div className="w-48">
+                <PlaceImg place={booking.place} />
+              </div>
+              <h2>{booking.place.title}</h2>
               {booking.checkIn} - {booking.checkOut}
             </div>
           ))}
